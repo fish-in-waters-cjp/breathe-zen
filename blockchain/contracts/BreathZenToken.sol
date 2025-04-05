d1c8d2bcc53cea522c67190292133aaa7cc8c32c
claire0613 <claire0711@gmail.com>
2025-04-05 17:51:27 +0800

    feat: add breathZenToken contract

---
+++ blockchain/contracts/BreathZenToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BreathZenToken is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("BreathZenToken", "BREATH") Ownable(initialOwner) {}
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
